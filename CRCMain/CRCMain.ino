dc063a5f036ae8418bf64b62d39fa90d8d1d1d6f
Civelier <civelier.algon@gmail.com>
2021-04-14 15:24:25 -0400

    Add project files.

---
+++ CRCMain/CRCMain.ino
@@ -0,0 +1,27 @@
+/*
+ Name:		CRCMain.ino
+ Created:	4/14/2021 3:10:42 PM
+ Author:	civel
+*/
+
+
+
+// the setup function runs once when you press reset or power the board
+#include <Wire.h>
+#include <Servo.h>
+#include <PIDController.h>
+#include <MPU6050.h>
+#include <Encoder.h>
+#include <CrcLib.h>
+#include <Adafruit_VL53L0X.h>
+#include <Adafruit_NeoPixel.h>
+void setup() 
+{
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop()
+{
+
+}
