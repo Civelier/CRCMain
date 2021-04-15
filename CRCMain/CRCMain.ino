bf371a273185dc4a4b7b0f9badc7df4d9b523859
Civelier <civelier.algon@gmail.com>
2021-04-15 12:47:33 -0400

    Communication is recieved

--- CRCMain/CRCMain.ino
+++ CRCMain/CRCMain.ino
@@ -4,7 +4,8 @@
  Author:	civel
 */
 
-
+#define LOCAL_ADDRESS 0x16
+#define REMOTE_ADDRESS 0x17
 
 // the setup function runs once when you press reset or power the board
 #include <Wire.h>
@@ -19,19 +20,71 @@
 #include "RemoteDeviceManager.h"
 #include "RemoteIncludes.h"
 #include "LinkedList.h"
-
+#include <RemoteLinearEncodedMotor.h>
+#include "Transmitter.h"
 
 //using namespace Crc;
 
+Remote::DeviceManager Manager(2, CRC_DIG_2, CRC_DIG_1);
+Remote::RemoteLinearEncodedMotor* motorCharriot;
+Remote::RemoteLinearEncodedMotor* motorWind;
 
+void CommandManager()
+{
+	if (!Serial.available()) return;
+	int command = Serial.parseInt();
+	switch (command)
+	{
+	case 1: // set speed
+	{
+		double v = Serial.parseFloat();
+		motorWind->SetPercent(v);
+	}
+	break;
+	case 2: // move pos
+	{
+		double d = Serial.parseFloat();
+		motorWind->MoveDistance(d, 1);
+	}
+	break;
+	default:
+		break;
+	}
+}
 
+int* myArray;
 void setup() 
 {
 	Crc::CrcLib::Initialize();
+	Serial.begin(115200);
+	while (!Serial);
+	Serial.println();
+
+	myArray = (int*)malloc(sizeof(int) * 2);
+	myArray[0] = 1;
+	myArray[1] = 2;
+
+	Remote::Transmit.Init();
+	Manager.Init();
+	motorCharriot = new Remote::RemoteLinearEncodedMotor(0);
+	motorWind = new Remote::RemoteLinearEncodedMotor(1);
+
+	Manager.AddDevice(motorCharriot);
+	Manager.AddDevice(motorWind);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop()
 {
 	Crc::CrcLib::Update();
+	CommandManager();
+	if (Manager.CheckReply())
+	{
+		uint8_t id = Remote::Transmit.GetReplyDeviceID();
+		if (id != 255)
+		{
+			Serial.print("Reply from: ");
+			Serial.println((int)id);
+		}
+	}
 }
--- MegaGood/MegaGood.ino
+++ MegaGood/MegaGood.ino
@@ -4,6 +4,10 @@
  Author:	civel
 */
 
+#define LOCAL_ADDRESS 0x16
+#define REMOTE_ADDRESS 0x17
+
+
 #include <Servo.h>
 #include <PIDController.h>
 #include <LinearEncoder.h>
@@ -11,16 +15,19 @@
 #include <Encoder.h>
 #include <LocalDeviceManager.h>
 #include "LinearEncodedMotor.h"
-
+#include "Wire.h"
+#include "Addresses.h"
+#include "Reciever.h"
 
 using namespace Local;
 
-DeviceManager Manager(2, 52);
+DeviceManager Manager = DeviceManager(2, 52);
 
 LinearEncoder* encoCharriot;
 LinearEncodedMotor* motorCharriot;
 LinearEncoder* encoWind;
 LinearEncodedMotor* motorWind;
+uint8_t Buffer[MaxMessageLength];
 
 #define Charriot 2
 #define CharriotPWM 4
@@ -30,6 +37,7 @@ LinearEncodedMotor* motorWind;
 void WindInt()
 {
 	encoWind->_update();
+	digitalWrite(13, digitalRead(Wind));
 }
 
 void CharriotInt()
@@ -49,6 +57,12 @@ void CommandManager()
 		motorWind->SetPercent(v);
 	}
 	break;
+	case 2: // move pos
+	{
+		double d = Serial.parseFloat();
+		motorWind->MoveDistance(d, 1);
+	}
+		break;
 	default:
 		break;
 	}
@@ -57,20 +71,47 @@ void CommandManager()
 // the setup function runs once when you press reset or power the board
 void setup()
 {
+	pinMode(13, OUTPUT);
+	Wire.begin(SlaveAddress);
+	Serial.begin(115200);
 	encoCharriot = new LinearEncoder(Charriot, 1);
 	encoWind = new LinearEncoder(Wind, 1);
 	motorCharriot = new LinearEncodedMotor(encoWind, CharriotPWM, 0);
 	motorWind = new LinearEncodedMotor(encoWind, WindPWM, 1);
+	
+	Manager.Init();
+	Reciever::GetInstance(&Manager);
+
 
 	Manager.AddDevice(motorCharriot);
 	Manager.AddDevice(motorWind);
 
 	motorCharriot->Begin();
 	motorWind->Begin();
+
+	pinMode(Charriot, INPUT_PULLUP);
+	pinMode(Wind, INPUT_PULLUP);
+
+	attachInterrupt(digitalPinToInterrupt(Charriot), &CharriotInt, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(Wind), &WindInt, CHANGE);
+
 }
 
 // the loop function runs over and over again until power down or reset
 void loop()
 {
+	Manager.Run();
 	CommandManager();
+	motorCharriot->Update();
+	motorWind->Update();
+	static uint32_t nextMillis = 0;
+	if (nextMillis < millis())
+	{
+		nextMillis = millis() + 500;
+		Serial.println(encoWind->GetPosition());
+	}
+	if (Reciever::GetInstance().RequestAvailable())
+	{
+		Serial.println("Request recieved");
+	}
 }
