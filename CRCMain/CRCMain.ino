bf0522e7019bad3e3e516195f3e36218c971559c
Civelier <civelier.algon@gmail.com>
2021-04-16 18:05:20 -0400

    Working again with new procedure

--- CRCMain/CRCMain.ino
+++ CRCMain/CRCMain.ino
@@ -24,168 +24,218 @@
 //using namespace Crc;
 
 
-CRCSchedulerClass Scheduler;
-Remote::DeviceManager Manager(2, CRC_DIG_2, CRC_DIG_1);
-Remote::RemoteLinearEncodedMotor* motorCharriot;
-Remote::RemoteLinearEncodedMotor* motorWind;
-Remote::RemoteLinearEncodedMotor* motorRotate;
+//CRCSchedulerClass Scheduler;
+//Remote::DeviceManager Manager(2, CRC_DIG_2, CRC_DIG_1);
+//Remote::RemoteLinearEncodedMotor* motorCharriot;
+//Remote::RemoteLinearEncodedMotor* motorWind;
+//Remote::RemoteLinearEncodedMotor* motorRotate;
 
-Bilda5202Motor* motorL;
-Bilda5202Motor* motorR;
+//Bilda5202Motor* motorL;
+//Bilda5202Motor* motorR;
 
-enum MotorGroupMode
+struct Motor
 {
-	Off,
-	HoldPosition,
-	MoveStraight,
-	TurnCentered,
-	OnStraight,
-	OnTurn,
-};
-
-struct MotorGroup
-{
-	MotorGroup(Bilda5202Motor* left, Bilda5202Motor* right)
+	Motor(uint8_t pin, bool inverse = false)
 	{
-		Left = left;
-		Right = right;
+		Pin = pin;
+		Inverse = inverse;
 	}
 	void Begin()
 	{
-		SpeedPID.begin();
-		SpeedPID.limit(-1, 1);
-		SpeedPID.setpoint(0);
-		SpeedPID.tune(1, 0, 0);
-		PosPID.begin();
-		PosPID.limit(-1, 1);
-		PosPID.tune(PID_VALUES_BILDA_188);
+		Crc::CrcLib::InitializePwmOutput(Pin);
 	}
-	void AdjustSpeedStraight()
+	void SetPercent(double value)
 	{
-		double ratio = SpeedPID.compute((Left->GetEncoderCount() - LeftStart) - (Right->GetEncoderCount() - RightStart));
-		double speed = PosPID.compute((Left->GetDegrees() + Right->GetDegrees()) / 2);
-		double l = (1 - ratio) * speed;
-		double r = (1 + ratio) * speed;
-		Left->SetSpeed(constrain(l, -MaxSpeed, MaxSpeed));
-		Right->SetSpeed(constrain(r, -MaxSpeed, MaxSpeed));
+		Crc::CrcLib::SetPwmOutputFastDouble(Pin, 0.5 + (Inverse ? -value : value) / 2);
 	}
-
-	void AdjustSpeedTurning()
-	{
-		double ratio = SpeedPID.compute((Left->GetEncoderCount() - LeftStart) + (Right->GetEncoderCount() - RightStart));
-		double speed = PosPID.compute((Left->GetDegrees() - Right->GetDegrees()) / 2);
-		double l = (1 - ratio) * speed;
-		double r = (1 + ratio) * -speed;
-		Left->SetSpeed(constrain(l, -1, 1));
-		Right->SetSpeed(constrain(r, -1, 1));
-	}
-
-	void AdjustSpeedGoStraight()
-	{
-		double ratio = SpeedPID.compute((Left->GetEncoderCount() - LeftStart) - (Right->GetEncoderCount() - RightStart));
-		double l = (1 - ratio) * MaxSpeed;
-		double r = (1 + ratio) * MaxSpeed;
-		Left->SetSpeed(constrain(l, -1, 1));
-		Right->SetSpeed(constrain(r, -1, 1));
-	}
-
-	void AdjustSpeedTurnForever()
-	{
-		double ratio = SpeedPID.compute((Left->GetEncoderCount() - LeftStart) + (Right->GetEncoderCount() - RightStart));
-		double speed = PosPID.compute((Left->GetDegrees() - Right->GetDegrees()) / 2);
-		double l = (1 - ratio) * speed;
-		double r = (1 + ratio) * -speed;
-		Left->SetSpeed(constrain(l, -1, 1));
-		Right->SetSpeed(constrain(r, -1, 1));
-	}
-
-	void Update()
-	{
-		switch (Mode)
-		{
-		case Off:
-			break;
-		case HoldPosition:
-			Left->UpdateSpeed();
-			Right->UpdateSpeed();
-			break;
-		case MoveStraight:
-			AdjustSpeedStraight();
-			break;
-		case TurnCentered:
-			AdjustSpeedTurning();
-			break;
-		case OnStraight:
-			AdjustSpeedGoStraight();
-			break;
-		default:
-			break;
-		}
-	}
-	void TurnOff()
-	{
-		Left->Off();
-		Right->Off();
-		Mode = MotorGroupMode::Off;
-	}
-
-	void Brake(bool overwrite = false)
-	{
-		Left->RotateDegrees(0, overwrite);
-		Right->RotateDegrees(0, overwrite);
-		Mode = MotorGroupMode::HoldPosition;
-	}
-
-	void OnGoStraight(double speed)
-	{
-		SpeedPID.limit(-speed, speed);
-		MaxSpeed = speed;
-		Mode = MotorGroupMode::OnStraight;
-		LeftStart = Left->GetEncoderCount();
-		RightStart = Right->GetEncoderCount();
-	}
-
-	void MoveDistance(double mm, double speed)
-	{
-		SpeedPID.limit(-speed, speed);
-		MaxSpeed = speed;
-		Mode = MotorGroupMode::MoveStraight;
-		Target = mm;
-		PosPID.setpoint(mm);
-		LeftStart = Left->GetEncoderCount();
-		RightStart = Right->GetEncoderCount();
-	}
-
-	void TurnDistance(double mm, double speed)
-	{
-		SpeedPID.limit(-speed, speed);
-		MaxSpeed = speed;
-		Mode = MotorGroupMode::TurnCentered;
-		Target = mm;
-		PosPID.setpoint(mm);
-		LeftStart = Left->GetEncoderCount();
-		RightStart = Right->GetEncoderCount();
-	}
-
-	void TurnForever(double speed)
-	{
-		SpeedPID.limit(-speed, speed);
-		MaxSpeed = speed;
-		Mode = MotorGroupMode::OnTurn;
-		LeftStart = Left->GetEncoderCount();
-		RightStart = Right->GetEncoderCount();
-	}
-
-	MotorGroupMode Mode;
-	double Target;
-	double MaxSpeed;
-	int32_t LeftStart;
-	int32_t RightStart;
-	Bilda5202Motor* Left;
-	Bilda5202Motor* Right;
-	PIDController SpeedPID;
-	PIDController PosPID;
+	uint8_t Pin;
+	bool Inverse;
 };
+
+struct LimitSwitch
+{
+	LimitSwitch(uint8_t pin, bool inverse = false)
+	{
+		Pin = pin;
+		Inverse = inverse;
+	}
+	void Begin()
+	{
+		Crc::CrcLib::SetDigitalPinMode(Pin, INPUT);
+	}
+	bool GetState()
+	{
+		return digitalRead(Pin) == (Inverse ? LOW : HIGH);
+	}
+	void Test()
+	{
+		Serial.print("state: ");
+		Serial.println(GetState());
+	}
+	uint8_t Pin;
+	bool Inverse;
+};
+
+Motor Pivot(CRC_PWM_9);
+Motor Vertical(CRC_PWM_2);
+Motor Horizontal(CRC_PWM_3);
+LimitSwitch PivotLS(CRC_DIG_3, true);
+LimitSwitch VerticalLow(CRC_DIG_4);
+LimitSwitch PivotHighLS(CRC_DIG_5);
+
+//enum MotorGroupMode
+//{
+//	Off,
+//	HoldPosition,
+//	MoveStraight,
+//	TurnCentered,
+//	OnStraight,
+//	OnTurn,
+//};
+
+//struct MotorGroup
+//{
+//	MotorGroup(Bilda5202Motor* left, Bilda5202Motor* right)
+//	{
+//		Left = left;
+//		Right = right;
+//	}
+//	void Begin()
+//	{
+//		SpeedPID.begin();
+//		SpeedPID.limit(-1, 1);
+//		SpeedPID.setpoint(0);
+//		SpeedPID.tune(1, 0, 0);
+//		PosPID.begin();
+//		PosPID.limit(-1, 1);
+//		PosPID.tune(PID_VALUES_BILDA_188);
+//	}
+//	void AdjustSpeedStraight()
+//	{
+//		double ratio = SpeedPID.compute((Left->GetEncoderCount() - LeftStart) - (Right->GetEncoderCount() - //RightStart));
+//		double speed = PosPID.compute((Left->GetDegrees() + Right->GetDegrees()) / 2);
+//		double l = (1 - ratio) * speed;
+//		double r = (1 + ratio) * speed;
+//		Left->SetSpeed(constrain(l, -MaxSpeed, MaxSpeed));
+//		Right->SetSpeed(constrain(r, -MaxSpeed, MaxSpeed));
+//	}
+//
+//	void AdjustSpeedTurning()
+//	{
+//		double ratio = SpeedPID.compute((Left->GetEncoderCount() - LeftStart) + (Right->GetEncoderCount() - //RightStart));
+//		double speed = PosPID.compute((Left->GetDegrees() - Right->GetDegrees()) / 2);
+//		double l = (1 - ratio) * speed;
+//		double r = (1 + ratio) * -speed;
+//		Left->SetSpeed(constrain(l, -1, 1));
+//		Right->SetSpeed(constrain(r, -1, 1));
+//	}
+//
+//	void AdjustSpeedGoStraight()
+//	{
+//		double ratio = SpeedPID.compute((Left->GetEncoderCount() - LeftStart) - (Right->GetEncoderCount() - //RightStart));
+//		double l = (1 - ratio) * MaxSpeed;
+//		double r = (1 + ratio) * MaxSpeed;
+//		Left->SetSpeed(constrain(l, -1, 1));
+//		Right->SetSpeed(constrain(r, -1, 1));
+//	}
+//
+//	void AdjustSpeedTurnForever()
+//	{
+//		double ratio = SpeedPID.compute((Left->GetEncoderCount() - LeftStart) + (Right->GetEncoderCount() - //RightStart));
+//		double speed = PosPID.compute((Left->GetDegrees() - Right->GetDegrees()) / 2);
+//		double l = (1 - ratio) * speed;
+//		double r = (1 + ratio) * -speed;
+//		Left->SetSpeed(constrain(l, -1, 1));
+//		Right->SetSpeed(constrain(r, -1, 1));
+//	}
+//
+//	void Update()
+//	{
+//		switch (Mode)
+//		{
+//		case Off:
+//			break;
+//		case HoldPosition:
+//			Left->UpdateSpeed();
+//			Right->UpdateSpeed();
+//			break;
+//		case MoveStraight:
+//			AdjustSpeedStraight();
+//			break;
+//		case TurnCentered:
+//			AdjustSpeedTurning();
+//			break;
+//		case OnStraight:
+//			AdjustSpeedGoStraight();
+//			break;
+//		default:
+//			break;
+//		}
+//	}
+//	void TurnOff()
+//	{
+//		Left->Off();
+//		Right->Off();
+//		Mode = MotorGroupMode::Off;
+//	}
+//
+//	void Brake(bool overwrite = false)
+//	{
+//		Left->RotateDegrees(0, overwrite);
+//		Right->RotateDegrees(0, overwrite);
+//		Mode = MotorGroupMode::HoldPosition;
+//	}
+//
+//	void OnGoStraight(double speed)
+//	{
+//		SpeedPID.limit(-speed, speed);
+//		MaxSpeed = speed;
+//		Mode = MotorGroupMode::OnStraight;
+//		LeftStart = Left->GetEncoderCount();
+//		RightStart = Right->GetEncoderCount();
+//	}
+//
+//	void MoveDistance(double mm, double speed)
+//	{
+//		SpeedPID.limit(-speed, speed);
+//		MaxSpeed = speed;
+//		Mode = MotorGroupMode::MoveStraight;
+//		Target = mm;
+//		PosPID.setpoint(mm);
+//		LeftStart = Left->GetEncoderCount();
+//		RightStart = Right->GetEncoderCount();
+//	}
+//
+//	void TurnDistance(double mm, double speed)
+//	{
+//		SpeedPID.limit(-speed, speed);
+//		MaxSpeed = speed;
+//		Mode = MotorGroupMode::TurnCentered;
+//		Target = mm;
+//		PosPID.setpoint(mm);
+//		LeftStart = Left->GetEncoderCount();
+//		RightStart = Right->GetEncoderCount();
+//	}
+//
+//	void TurnForever(double speed)
+//	{
+//		SpeedPID.limit(-speed, speed);
+//		MaxSpeed = speed;
+//		Mode = MotorGroupMode::OnTurn;
+//		LeftStart = Left->GetEncoderCount();
+//		RightStart = Right->GetEncoderCount();
+//	}
+//
+//	MotorGroupMode Mode;
+//	double Target;
+//	double MaxSpeed;
+//	int32_t LeftStart;
+//	int32_t RightStart;
+//	Bilda5202Motor* Left;
+//	Bilda5202Motor* Right;
+//	PIDController SpeedPID;
+//	PIDController PosPID;
+//};
 
 struct ServoMotor
 {
@@ -233,7 +283,9 @@ struct Pince
 	}
 };
 
-Pince pince(CRC_PWM_1, CRC_PWM_2, 1, -1);
+Pince pince(CRC_PWM_5, CRC_PWM_6, -1, 1);
+CRCSchedulerClass Scheduler;
+Remote::DeviceManager Manager(2, CRC_DIG_2, CRC_DIG_1);
 
 void CommandManager()
 {
@@ -244,13 +296,13 @@ void CommandManager()
 	case 1: // set speed
 	{
 		double v = Serial.parseFloat();
-		motorWind->SetPercent(v);
+		//motorWind->SetPercent(v);
 	}
 	break;
 	case 2: // move pos
 	{
 		double d = Serial.parseFloat();
-		motorWind->MoveDistance(d, 1);
+		//motorWind->MoveDistance(d, 1);
 	}
 	break;
 	case 3: // move pince
@@ -269,7 +321,7 @@ void setup()
 {
 	Crc::CrcLib::Initialize();
 	Serial.begin(115200);
-	while (!Serial);
+	//while (!Serial);
 	Serial.println();
 
 	myArray = (int*)malloc(sizeof(int) * 2);
@@ -278,18 +330,91 @@ void setup()
 
 	Remote::Transmit.Init();
 	Manager.Init();
-	motorCharriot = new Remote::RemoteLinearEncodedMotor(0);
-	motorWind = new Remote::RemoteLinearEncodedMotor(1);
+	//motorCharriot = new Remote::RemoteLinearEncodedMotor(0);
+	//motorWind = new Remote::RemoteLinearEncodedMotor(1);
+	//SETUP_DRIVING();
+	Pivot.Begin();
+	Vertical.Begin();
+	Horizontal.Begin();
+	PivotLS.Begin();
+	PivotHighLS.Begin();
+	VerticalLow.Begin();
 	pince.Init();
-	Manager.AddDevice(motorCharriot);
-	Manager.AddDevice(motorWind);
+	//Manager.AddDevice(motorCharriot);
+	//Manager.AddDevice(motorWind);
+}
+
+#define WaitForBegin(condition) while (!(condition)) { Crc::CrcLib::Update();
+
+#define WaitForEnd }
+
+#define Debug(text) Serial.println(text)
+
+void EasyRun()
+{
+	Debug("Start");
+	Pivot.SetPercent(0.2);
+	/*uint8_t s0 = millis() + 750;
+	WaitForBegin(millis() > s0);
+	WaitForEnd;*/
+	//Debug("Up");
+	//Pivot.SetPercent(0);
+	WaitForBegin(PivotLS.GetState());
+	WaitForEnd;
+	Debug("Pivot down");
+	Pivot.SetPercent(0);
+	/*uint32_t s1 = millis() + 1000;
+	WaitForBegin(millis() > s1);
+	WaitForEnd;*/
+	Debug("Stable");
+	pince.Open();
+	uint32_t s2 = millis() + 2000;
+	WaitForBegin(millis() > s2);
+	WaitForEnd;
+	Debug("Open");
+	Pivot.SetPercent(-0.25);
+	WaitForBegin(PivotHighLS.GetState());
+	WaitForEnd;
+
+	/*uint32_t s3 = millis() + 7000;
+	WaitForBegin(millis() > s3);
+	WaitForEnd;*/
+	Debug("Up");
+	Pivot.SetPercent(0);
+
 }
 
 // the loop function runs over and over again until power down or reset
 void loop()
 {
 	Crc::CrcLib::Update();
-	CommandManager();
+
+	if (digitalRead(CRC_DIG_12))
+	{
+		CommandManager();
+	}
+	else
+	{
+		Serial.println("ARMED!!!!!");
+		/*while (!digitalRead(CRC_DIG_1));
+		while (digitalRead(CRC_DIG_1));
+
+		Serial.println("resetting");
+		Pivot.SetPercent(-0.2);
+		while (Serial.available()) Serial.read();
+		while (!Serial.available() && !PivotHighLS.GetState())
+		{
+		}
+		Pivot.SetPercent(0);*/
+
+		while (!digitalRead(CRC_DIG_1));
+		while (digitalRead(CRC_DIG_1));
+		EasyRun();
+		while (!digitalRead(CRC_DIG_1));
+		while (digitalRead(CRC_DIG_1));
+	}
+	if (PivotLS.GetState()) Pivot.SetPercent(0);
+	if (PivotHighLS.GetState()) Pivot.SetPercent(0);
 	if (Manager.CheckReply())
 	{
 		uint8_t id = Remote::Transmit.GetReplyDeviceID();
@@ -299,4 +424,15 @@ void loop()
 			Serial.println((int)id);
 		}
 	}
+
+	if (Manager.CheckReply())
+	{
+		uint8_t id = Remote::Transmit.GetReplyDeviceID();
+		if (id != 255)
+		{
+			Serial.print("Reply from: ");
+			Serial.println((int)id);
+		}
+	}
+
 }
